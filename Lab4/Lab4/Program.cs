4f3638734c07af4928ff86410b097c052c00912e
Wuja33 <dawid.szelag22@gmail.com>
2021-12-08 17:11:05 +0100

    Dodanie Pracydomowej4

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab4
+{
+    class Program
+    {
+        /// <summary>
+        /// Funkcja, która gratuluje użytkownikowi
+        /// </summary>
+        static void Congratulations(string name,int age = -1)
+        {
+            Console.WriteLine("Gratulacje " + name + "! Napisałeś pierwszą funkcję! Masz " + age + " lat. ");
+        }
+        /// <summary>
+        /// Funkcja obliczająca długość okręgu
+        /// </summary>
+        /// <param name="radius">Promień okręgu</param>
+        static void CalculateCircleLength(double radius)
+        {
+            Console.WriteLine($"Długość okręgu to: {Math.Round(2*radius*Math.PI,2)} ");
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Suma x i y</returns>
+        static int Add(int x,int y)
+        {
+            int z = x + y;
+            return z;
+        }
+        /// <summary>
+        /// Obliczanie pola
+        /// </summary>
+        /// <param name="radius">Promień</param>
+        /// <returns>pole</returns>
+        static double field(double radius)
+        {
+            return Math.Round(Math.Pow(radius,2)*Math.PI,2);
+        }
+        
+        static bool przestepny(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0) return true;
+                    else return false;
+                }
+                else return true;
+            }
+            else return false;
+        }
+
+        static List<string> fruit(List<string> listf)
+        {
+            var list1 = listf.Where(owoc => owoc.Length < 6 && owoc.StartsWith('l')).OrderBy(owoc => owoc).ToList();
+            return list1;
+        }
+        static void Main(string[] args)
+        {
+            /*
+            Program.Congratulations("Dawid",19);
+            Congratulations("Dawid");
+            CalculateCircleLength(3);
+            Console.WriteLine(Add(1,3));
+            
+            Console.WriteLine($"bool wynosi: {przestepny(2012)}");
+            */
+
+            List<string> owoce = new List<string>() { "limonka","liczi","laskowiec" };
+            
+
+            fruit(owoce);
+            foreach (string item in fruit(owoce))
+            {
+                Console.WriteLine(item);
+            }
+
+            /*
+            int[] scores = new int[] { 97, 60, 10, 20, 71, 80 ,85, 100};
+
+            var scoresQuery = from score in scores where score > 70 select score;
+            
+            foreach (int score in scoresQuery)
+            {
+                Console.WriteLine(score);
+            }
+
+            var scoresQueryMethodBased = scores.Where(score => score > 70).OrderBy(score => score).ToList();
+
+            foreach (int score in scoresQueryMethodBased)
+            {
+                Console.WriteLine(score);
+            }
+
+            var wordsA = new string[] { "apple", "peach", "blueberry" };
+
+            var wordsB = new string[] { "apple", "peach", "blueberry" };
+
+            bool match = wordsA.SequenceEqual(wordsB);
+
+            Console.WriteLine(match);
+
+            List<int> testList = Enumerable.Range(0, 1000).ToList();
+
+            var max = testList.Max();
+
+            var min = testList.Min();
+
+            var avarage = testList.Average();
+
+            var firstOrDefault = testList.FirstOrDefault(number => number > 99);
+            */
+        }
+    }
+}
---
+++ Zadania domowe/Zadaniedomowe4/Zadaniedomowe4/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+
+namespace Zadaniedomowe4
+{
+    class Program
+    {
+        static void testresult(double procent)
+        {
+            if (procent < 30.0) Console.WriteLine("Ocena: 1");
+            else if (procent >= 30.0 && procent <= 50.0) Console.WriteLine("Ocena: 2");
+            else if (procent > 50.0 && procent < 75.0) Console.WriteLine("Ocena: 3");
+            else if (procent >= 75.0 && procent <= 90.0) Console.WriteLine("Ocena: 4");
+            else if (procent > 90.0 && procent < 100.0) Console.WriteLine("Ocena: 5");
+            else if (procent == 100.0) Console.WriteLine("Ocena: 6");
+        }
+        static double Tax(double income)
+        {
+            if (income <= 85_528.0) return (income * 0.17);
+            else if (income > 85_528.0 && income < 1_000_000.0) return (85_528.0 * 0.17 + (income- 85_528.0) * 0.32);
+            else return (85_528.0 * 0.17 + (1_000_000.0 - 85_528.0)* 0.32 + (income - 1_000_000.0) * 0.36);
+        }
+        static double calculator(double x, double y, char z)
+        {
+            switch(z)
+            {
+                case '+':
+                    {
+                        return (x + y);
+                        break;
+                    }
+                case '-':
+                    {
+                        return (x - y);
+                        break;
+                    }
+                case '*':
+                    {
+                        return (x * y);
+                        break;
+                    }
+                case '/':
+                    {
+                        return (x / y);
+                        break;
+                    }
+                default:
+                    return 0;
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            double procentest;
+            bool bug = false;
+            do
+            {
+                Console.WriteLine("Podaj procent z testu (0-100): ");
+                if (double.TryParse(Console.ReadLine(), out procentest) && procentest >= 0 && procentest <= 100)
+                {
+                    testresult(procentest);
+                    bug = false;
+                }else
+                {
+                    Console.WriteLine("Podałeś niepoprawną daną! Podaj ponownie.\n\n ");
+                    bug = true;
+                }
+                
+            } while (bug == true);
+
+            double income;
+            do
+            {
+                Console.WriteLine("Podaj dochód: ");
+                if (double.TryParse(Console.ReadLine(), out income) && income > 0)
+                {
+                    Console.WriteLine($"Twój podatek wynosi: {Tax(income)}"); 
+                    bug = false;
+                }
+                else
+                {
+                    Console.WriteLine("Podałeś niepoprawną daną! Podaj ponownie.\n\n ");
+                    bug = true;
+                }
+            } while (bug == true);
+
+            char sign;
+            bool exitcalc = false;
+            double number1, number2;
+            do
+            {
+                Console.WriteLine("KALKULATOR\n");
+                Console.WriteLine("Podaj 2 liczby oraz operator ( - ;  + ; * ; / ): \n");
+                do
+                {
+                    Console.WriteLine("Podaj 1 liczbe: ");
+                    if (double.TryParse(Console.ReadLine(), out number1)) bug = false;
+                    else
+                    {
+                        Console.WriteLine("Podałeś niepoprawną daną! Podaj ponownie.\n\n ");
+                        bug = true;
+                    }
+
+                } while (bug == true);
+
+                do
+                {
+                    Console.WriteLine("Podaj 2 liczbe: ");
+                    if (double.TryParse(Console.ReadLine(), out number2)) bug = false;
+                    else
+                    {
+                        Console.WriteLine("Podałeś niepoprawną daną! Podaj ponownie.\n\n ");
+                        bug = true;
+                    }
+
+                } while (bug == true);
+
+                do
+                {
+                    Console.WriteLine("Podaj znak: ");
+                    if (char.TryParse(Console.ReadLine(), out sign) && (sign == '-' || sign == '+' || sign == '*' || sign == '/'))
+                    {
+                        if (number2 == 0.0 && sign == '/')
+                        {
+                            Console.WriteLine("Nie można dzielić przez zero!");
+                            break;
+                        }
+                        Console.WriteLine("Wynik działania: {0}", calculator(number1, number2, sign));
+                        bug = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Podałeś niepoprawną daną! Podaj ponownie.\n\n ");
+                        bug = true;
+                    }
+                } while (bug == true);
+                Console.WriteLine("Aby wykonać kolejne działanie, wciśnij 1. Aby zakończyc program wciśnij 0.");
+
+                do
+                {
+                    Console.WriteLine("Podaj 1 lub 0: ");
+                    if (char.TryParse(Console.ReadLine(), out sign) && (sign == '1' || sign == '0'))
+                    {
+                        if (sign == '1')
+                        {
+                            bug = false;
+                            exitcalc = false;
+                        }
+                        else if (sign == '0') Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Podałeś niepoprawną daną! Podaj ponownie.\n\n ");
+                        bug = true;
+                    }
+                } while (bug == true);
+                Console.Clear();
+            } while (exitcalc == false);
+        }
+    }
+}
